cf6a87820764d41a5d9ac82f94beea2958489955
Sumchenko <nik.sumchenko.2004@mail.ru>
2024-07-07 15:48:44 +0300

    добавлена обработка ошибок

--- src/main/java/ag/selm/manager/controller/ProductsController.java
+++ src/main/java/ag/selm/manager/controller/ProductsController.java
@@ -3,10 +3,15 @@ package ag.selm.manager.controller;
 import ag.selm.manager.controller.payload.NewProductPayload;
 import ag.selm.manager.service.ProductService;
 import ag.selm.manager.entity.Product;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.naming.Binding;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,11 +32,21 @@ public class ProductsController {
     }
 
     @PostMapping("create")
-    public String createProduct(NewProductPayload payload) {
-        Product product = this.productService.createProduct(payload.title(), payload.details());
-        return "redirect:/catalogue/products/list".formatted(product.getId());
-    }
+    public String createProduct(@Valid NewProductPayload payload,
+                                BindingResult bindingResult,
+                                Model model) {
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/list".formatted(product.getId());
+        }
 
+    }
 
 
 }
